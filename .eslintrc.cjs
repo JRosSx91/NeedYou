35dac52c9082de6e57b36a5d149df128366c6e84
JRosSx91 <hazee.1491@gmail.com>
2024-03-17 15:10:43 +0100

    changes

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    extends: [
+      '@nuxt/eslint-config',
+      '@nuxtjs/eslint-config-typescript'
+    ],
+    parserOptions: {
+      parser: '@typescript-eslint/parser'
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'max-len': ['error', { code: 80 }],
+      'vue/multi-word-component-names': 'off',
+      'vue/no-multiple-template-root': 'off',
+      'vue/singleline-html-element-content-newline': ['error', {
+        ignoreWhenNoAttributes: false,
+        ignoreWhenEmpty: true
+      }],
+      'vue/multiline-html-element-content-newline': ['error', {
+        ignoreWhenEmpty: true
+      }],
+      'vue/require-default-prop': 'off'
+    }
+  };
+  
