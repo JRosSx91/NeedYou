55631e9393550b2e1d35d8de0e477762ade772cd
JRosSx91 <hazee.1491@gmail.com>
2024-03-17 15:30:54 +0100

    installing modules

--- components/AlertMap.vue
+++ components/AlertMap.vue
@@ -32,7 +32,4 @@ export default {
 };
 </script>
 
-<style>
-/* Asegúrate de incluir el CSS de Leaflet aquí o en tu archivo nuxt.config para evitar problemas de estilos */
-@import "leaflet/dist/leaflet.css";
-</style>
+<style></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui", "@pinia/nuxt", "@nuxtjs/eslint-module", "@nuxtjs/i18n"],
+  modules: [
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    "@nuxtjs/eslint-module",
+    "@nuxtjs/i18n",
+    "@pinia-plugin-persistedstate/nuxt",
+    "nuxt3-leaflet",
+  ],
   devtools: { enabled: true },
 });
