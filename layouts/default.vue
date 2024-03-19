5197b7bbbb1662d215ebd91e4f5fe9a7ad894b35
JRosSx91 <hazee.1491@gmail.com>
2024-03-19 16:10:17 +0100

    creating default layout & footer/header components

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <header />
+  <slot />
+  <footer />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,11 @@
 <template>
-  <div class="container mx-auto px-4">
-    <h1 class="text-2xl font-bold text-center my-8">
-      Bienvenido a la Aplicación de Alertas
-    </h1>
-    <div class="flex justify-center">
-      <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold
-          py-2 px-4 rounded"
-        @click="emitirAlerta"
-      >
-        Emitir Alerta
-      </button>
-    </div>
-    <div>
-      <AlertForm />
-    </div>
+  <div>
+    <AlertForm />
   </div>
 </template>
 
 <script setup>
 import AlertForm from '~/components/AlertForm.vue';
-
-function emitirAlerta () {
-  console.log('Emitir alerta');
-}
 </script>
 
 <style></style>
