49f50d6f3b20e1a2faa92157e4b04bad43658ba9
JRosSx91 <hazee.1491@gmail.com>
2024-03-19 16:13:54 +0100

    adding header & footer components to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,13 @@
 <template>
-  <header />
+  <header>
+    <HeaderBar />
+  </header>
   <slot />
-  <footer />
+  <footer>
+    <FooterBar />
+  </footer>
 </template>
+<script setup lang="ts">
+import HeaderBar from '~/components/common/HeaderBar.vue';
+import FooterBar from '~/components/common/FooterBar.vue';
+</script>
