0d7c2f80d26af440f00bb5f46256e473cac48ae6
JRosSx91 <hazee.1491@gmail.com>
2024-03-20 01:05:03 +0100

    creating error.vue

---
+++ error.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+defineProps({
+  error: {
+    type: Object as PropType<NuxtError>,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h1>
+      {{ error.statusCode }}
+    </h1>
+    <NuxtLink to="/">
+      Go back home
+    </NuxtLink>
+  </div>
+</template>
