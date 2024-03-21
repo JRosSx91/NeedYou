d51ac83f62386b5f9d8476eab97f713a19faf672
JRosSx91 <hazee.1491@gmail.com>
2024-03-21 17:09:16 +0100

    adding more color btn to modal

--- components/common/ColorBtn.vue
+++ components/common/ColorBtn.vue
@@ -9,8 +9,20 @@
       <div class="p-4">
         <Placeholder class="h-20 w-48">
           <UButton
-            class="rounded-full bg-[#00C16A] dark:bg-[#00C16A]"
+            class="rounded-full bg-[#00C16A] dark:bg-[#00DC82]"
             @click="() => theme.ui.primary = 'green'"
+          />
+          <UButton
+            class="rounded-full bg-[#338dc2] dark:bg-[#59a8d7]"
+            @click="() => theme.ui.primary = 'curiousBlue'"
+          />
+          <UButton
+            class="rounded-full bg-[#11c7c1] dark:bg-[#29d8cf]"
+            @click="() => theme.ui.primary = 'eggBlue'"
+          />
+          <UButton
+            class="rounded-full bg-[#ffa500] dark:bg-[#ffc71b]"
+            @click="() => theme.ui.primary = 'orange'"
           />
         </Placeholder>
       </div>
