e0ee246d2680625b75c5368e74e4e2ef5e35b269
JRosSx91 <hazee.1491@gmail.com>
2024-03-23 13:32:11 +0100

    create check-mail page and add template

---
+++ pages/check-mail.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="sm:py-18 container relative mx-auto
+    px-6 py-16 md:py-24 lg:px-16 lg:py-24 xl:px-20
+    pt-8 md:pt-16 overflow-hidden"
+  >
+    <div class="relative">
+      <div
+        class="mx-auto max-w-2xl lg:col-span-6 lg:flex
+        lg:items-center justify-center text-center"
+      >
+        <div
+          class="relative z-10 lg:h-auto pt-[90px] lg:pt-[90px]
+        lg:min-h-[300px] flex flex-col items-center justify-center
+        sm:mx-auto md:w-3/4 lg:mx-0 lg:w-full gap-4 lg:gap-8"
+        >
+          <div class="flex flex-col items-center">
+            <h1
+              class="text-foreground text-4xl sm:text-5xl
+              sm:leading-none lg:text-7xl"
+            >
+              Please check your email inbox for a link
+              <span class=" text-primary-500 dark:text-primary-400">
+                to verify your account</span>
+              .
+            </h1>
+            <p
+              class="pt-2
+                text-foreground my-3 text-sm
+              sm:mt-5 lg:mb-0 sm:text-base lg:text-lg"
+            >
+              If you have not yet received the verification
+              email you can resend it from this link.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
