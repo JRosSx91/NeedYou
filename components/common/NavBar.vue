0d79034292dd2c966a1dcc52262d3535505a4dd3
JRosSx91 <hazee.1491@gmail.com>
2024-03-25 16:34:04 +0100

    creating more components on header

--- components/common/NavBar.vue
+++ components/common/NavBar.vue
@@ -1,13 +1,34 @@
 <template>
   <ul class="items-center gap-x-8 hidden lg:flex font-primary">
     <li>
-      {{ $t('header.nav.navLink1') }}
-    </li>
-    <li class="text-primary-500 dark:text-primary-400">
-      {{ $t('header.nav.navLink2') }}
+      <ULink
+        to="/"
+        active-class="text-primary"
+        inactive-class="text-gray-500 dark:text-gray-400
+        hover:text-gray-700 dark:hover:text-gray-200"
+      >
+        {{ $t('header.nav.navLink1') }}
+      </ULink>
     </li>
     <li>
-      {{ $t('header.nav.navLink3') }}
+      <ULink
+        to="/how-it-works"
+        active-class="text-primary"
+        inactive-class="text-gray-500 dark:text-gray-400
+        hover:text-gray-700 dark:hover:text-gray-200"
+      >
+        {{ $t('header.nav.navLink2') }}
+      </ULink>
+    </li>
+    <li>
+      <ULink
+        to="/about"
+        active-class="text-primary"
+        inactive-class="text-gray-500 dark:text-gray-400
+        hover:text-gray-700 dark:hover:text-gray-200"
+      >
+        {{ $t('header.nav.navLink3') }}
+      </ULink>
     </li>
   </ul>
 </template>
